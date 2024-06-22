7aca05f66676c63a77935ad439ad3d18341b3c4c
Vitória Calvi <calvi.vitoria@gmail.com>
2024-06-22 18:04:05 -0300

    add model, view and controller for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
+  get "/posts", to: "posts#index"
 
   get "/home", to: "home#index"
 end
--- test/controllers/tabs_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class TabsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get tabs_index_url
-    assert_response :success
-  end
-end
