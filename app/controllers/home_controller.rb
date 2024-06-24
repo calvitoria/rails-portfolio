f297afb64d6f01ecc8669307dc5ce7512c6dc18d
Vitória Calvi <calvi.vitoria@gmail.com>
2024-06-24 09:44:22 -0300

    add about tab content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def feed
+    @posts = Post.all
+  end
+
+  def about
+    # Action for the About tab
+  end
+
+  def projects
+    # Action for the Projects tab
+  end
+
+  def guestbook
+    # Action for the Guestbook tab
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
   get "/posts", to: "posts#index"
+  get 'home/feed', to: 'home#feed'
+  get 'home/about', to: 'home#about'
+  get 'home/projects', to: 'home#projects'
+  get 'home/guestbook', to: 'home#guestbook'
 
   get "/home", to: "home#index"
 end
